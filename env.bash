b99a1a153df1a8754cdc9651cc43a5de336eb5d9
User Catdog <catdog@seahorse.nix.bra>
2014-12-10 14:57:42 +0800

    use bash scripts to record compile switches

---
+++ env.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+if [ -d build ]; then
+  rm -rf build;
+fi
+
+mkdir build
+cd build
+
+export CC=gcc48
+export CXX=g++48
+export CPP=cpp48
+
+
+export LDFLAGS="-lexecinfo -lkvm"
+export CXXFLAGS="-lexecinfo -lkvm"
+
+cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_EXE_LINKER_FLAGS='-lexecinfo -lkvm' \
+        -DCMAKE_AR=/usr/local/bin/gcc-ar48 \
+	-DCMAKE_RANLIB=/usr/local/bin/gcc-ranlib48 \
+	-DTOKU_DEBUG_PARANOID=OFF \
+	-DUSE_VALGRIND=OFF \
+	-DUSE_BDB=OFF \
+	-DUSE_SSL=OFF \
+	-DUSE_SYSTEM_BOOST=ON \
+	-DUSE_SYSTEM_PCRE=ON \
+	-DUSE_SYSTEM_V8=OFF \
+	-DUSE_CTAGS=OFF \
+	-DUSE_ETAGS=OFF \
+	-DUSE_GTAGS=OFF \
+	-DUSE_CSCOPE=OFF \
+	-DBUILD_TESTING=OFF \
+	-DTOKUMX_DISTNAME=2.0.0 ..
+
